0f965010088ba3941024b4b9521bdee7fff6109d
Nkanu Jackson Julius <61075333+Deminifah@users.noreply.github.com>
2024-11-12 08:03:17 +0100

    Bug Fixed...

--- app/src/main/java/com/deminifah/deminiccalc/screen/UnitConverter.kt
+++ app/src/main/java/com/deminifah/deminiccalc/screen/UnitConverter.kt
@@ -2,7 +2,6 @@ package com.deminifah.deminiccalc.screen
 
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.fillMaxSize
@@ -12,12 +11,7 @@ import androidx.compose.foundation.lazy.LazyRow
 import androidx.compose.foundation.lazy.grid.GridCells
 import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
 import androidx.compose.foundation.lazy.grid.itemsIndexed
-import androidx.compose.material3.Icon
-import androidx.compose.material3.OutlinedTextField
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.colorResource
 import androidx.compose.ui.res.painterResource
