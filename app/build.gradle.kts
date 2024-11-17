734c0b32d39b6e707c981aa19d01570135523e58
Nkanu Jackson Julius <61075333+Deminifah@users.noreply.github.com>
2024-11-17 20:13:46 +0100

    History Button Worked..

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -13,8 +13,8 @@ android {
         applicationId = "com.deminifah.deminiccalc"
         minSdk = 24
         targetSdk = 35
-        versionCode = 1
-        versionName = "2.0"
+        versionCode = 2
+        versionName = "1.1"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {
